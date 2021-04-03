4b46a37e7f8a8c049092fdb65aa3be8a3482bd11
jai-dewani <jai.dewani.99@gmail.com>
2021-04-03 22:23:38 +0530

    Update dist as well

--- dist/index.js
+++ dist/index.js
@@ -39218,7 +39218,9 @@ const skipAction = (payload) => {
 
 async function run() {
     try {
-        const payload = JSON.stringify(github.context.payload, undefined, 2);
+        const payload_out = JSON.stringify(github.context.payload, undefined, 2);
+        const payload = github.context.payload;
+        console.log(payload_out)
         var tweetContent = core.getInput('tweet');
         var tweet;
         try{
@@ -39267,6 +39269,8 @@ const asyncStringReplace = async (str, regex, obj, aReplacer) => {
     let match;
     let i = 0;
     while ((match = regex.exec(str)) !== null) {
+        console.log("Inside Match",match);
+        console.log("Inside Match",obj);
         substrs.push(str.slice(i, match.index));
         substrs.push(await aReplacer(obj, match[1]));
         i = regex.lastIndex;
@@ -39276,16 +39280,21 @@ const asyncStringReplace = async (str, regex, obj, aReplacer) => {
 };
 
 const index = async(obj,p1) => {
-    if(typeof p1 == 'string' && p1 =='twitter_username')
+    if(typeof p1 == 'string' && p1 =='twitter_username'){
+        console.log("Inside Index, Twitter username",obj,p1)        
         return await extractTwitterHandle(obj);
+    }
     else{
         if (typeof p1 == 'string')
             p1=p1.split('.');
             
+        console.log("Inside Index",obj,p1)        
         while(p1 && p1.length>0){
             obj = obj[p1[0]]
             p1 = p1.slice(1);
+            console.log("Inside Index",obj,p1)        
         }
+        console.log(obj);
         return obj;
     } 
 }
