2756553c33a5a3bd4e44466b209adfd2aed1fb50
jai-dewani <jai.dewani.99@gmail.com>
2021-04-03 22:16:49 +0530

    Add logs to renderText

--- src/renderText.js
+++ src/renderText.js
@@ -9,6 +9,8 @@ const asyncStringReplace = async (str, regex, obj, aReplacer) => {
     let match;
     let i = 0;
     while ((match = regex.exec(str)) !== null) {
+        console.log("Inside Match",match);
+        console.log("Inside Match",obj);
         substrs.push(str.slice(i, match.index));
         substrs.push(await aReplacer(obj, match[1]));
         i = regex.lastIndex;
@@ -18,16 +20,21 @@ const asyncStringReplace = async (str, regex, obj, aReplacer) => {
 };
 
 const index = async(obj,p1) => {
-    if(typeof p1 == 'string' && p1 =='twitter_username')
+    if(typeof p1 == 'string' && p1 =='twitter_username'){
+        console.log("Inside Index, Twitter username",obj,p1)        
         return await extractTwitterHandle(obj);
+    }
     else{
         if (typeof p1 == 'string')
             p1=p1.split('.');
             
+        console.log("Inside Index",obj,p1)        
         while(p1 && p1.length>0){
             obj = obj[p1[0]]
             p1 = p1.slice(1);
+            console.log("Inside Index",obj,p1)        
         }
+        console.log(obj);
         return obj;
     } 
 }
